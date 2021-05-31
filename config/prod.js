54f24c987f5bf6e95238d376ce8d1fb2f8c41848
MSH <saqlainm328@gmail.com>
2021-05-31 17:25:17 +0500

    first commit

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,10 @@
-module.exports = {
-    JWT_SECRET : process.env.JWT_SECRET,
-    MONGOURI : process.env.MONGOURI
+// module.exports = {
+//     JWT_SECRET : process.env.JWT_SECRET,
+//     MONGOURI : process.env.MONGOURI
     
+// }
+module.exports = {
+    JWT_SECRET : "Zohaib809@",
+    MONGOURI : 'mongodb+srv://Zohaib:Zohaib809@cluster0.dvsg1.mongodb.net/tododb?retryWrites=true&w=majority'
 }
+
